6ab9dfe00aeef31e333792b784053eda7620270f
Alisson Macedo <alisson.macedo@alfreddelivery.com>
2023-04-13 20:50:40 -0300

    feat: add new textArea and TextInput

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,33 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextInput, TextInputProps } from '@alissonmacedo/react'
+
+export default {
+  title: 'TextArea/Text',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text>Observations</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Add any observations',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
